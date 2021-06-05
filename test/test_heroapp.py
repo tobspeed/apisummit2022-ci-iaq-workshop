cca369de888ff895c8c653e4d2185a457e830f1d
sparsick <sparsick@web.de>
2021-06-05 18:18:42 +0200

    solution 02

--- test/test_heroapp.py
+++ test/test_heroapp.py
@@ -1,11 +1,16 @@
 def test_docker_is_installed(host):
-    # testen, ob das Paket "docker-ce" installiert ist
+    docker = host.package("docker-ce")
+    assert docker.is_installed
 
 def test_docker_service_is_running(host):
-    # testen, ob der Service "docker" läuft und verfügbar ist
+    dockerd = host.service("docker")
+    assert dockerd.is_enabled
+    assert dockerd.is_running
 
 def test_heroapp_container_is_running(host):
-    # testen, ob der container mit dem Namen "my-hero-app" läuft
+    heroapp = host.docker("my-hero-app")
+    assert heroapp.is_running
 
 def test_heroapp_is_available_on_port_80(host):
-    # testen, ob auf tcp://0.0.0.0:80 gehorcht wird
+    heroapp = host.socket("tcp://0.0.0.0:80")
+    assert heroapp.is_listening
