52be893af093aea98fa8ca15fda9999dd0ccdd81
TobiasBraumann <tobias.braumann@deutschebahn.com>
2022-06-20 15:44:07 +0200

    fix test script

--- ci-test-vm/main.tf
+++ ci-test-vm/main.tf
@@ -4,7 +4,7 @@ provider "hcloud" {
 }
 
 # Create a new SSH key
-resource "hcloud_ssh_key" "ansible-test-ssh-key" {
+resource "hcloud_ssh_key" "ansible-test-ssh-key2" {
   name = "Ansible Test VM SSH Key3"
   public_key = file(var.ssh_key)
 }
